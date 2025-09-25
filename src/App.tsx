1066de09d93f1d863084242f1e1b1920790626b1
Himanshu Verma <145604241+Hverma1206@users.noreply.github.com>
2025-09-25 17:40:36 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import MenuBar from './components/MenuBar';
 import Toolbar from './components/Toolbar';
 import NavigationTree from './components/NavigationTree';
 import EditorArea from './components/EditorArea';
-import ConsolePanel from './components/ConsolePanel';
 
 interface Tab {
   id: string;
