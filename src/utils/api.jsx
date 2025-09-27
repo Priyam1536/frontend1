96cb64cd5151054fd9cd2f8856a1831aa9d85074
Himanshu <hv308489@gmail.com>
2025-09-27 14:28:35 +0530

    api

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://salem-mario-lease-untitled.trycloudflare.com/api';
 
 // Auth related API calls
 export const authAPI = {
